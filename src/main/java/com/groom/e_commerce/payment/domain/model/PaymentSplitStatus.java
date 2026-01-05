e1238359c8954d5e60159e6e48e8d3826f8dadf4
yseoy0311 <yseoy0311@gmail.com>
2026-01-05 14:16:44 +0900

    feat(payment): PaymentSplitStatus enum 추가 (#48)

---
+++ src/main/java/com/groom/e_commerce/payment/domain/model/PaymentSplitStatus.java
@@ -0,0 +1,8 @@
+// src/main/java/com/groom/e_commerce/payment/domain/model/PaymentSplitStatus.java
+package com.groom.e_commerce.payment.domain.model;
+
+public enum PaymentSplitStatus {
+	PAID,
+	PARTIAL_CANCELLED,
+	CANCELLED
+}
