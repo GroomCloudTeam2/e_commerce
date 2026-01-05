2f36272e82bf57fdfa1a6c5667dc1a962d495e1b
yseoy0311 <yseoy0311@gmail.com>
2026-01-05 17:28:19 +0900

    feat(payment): 주문상품 부분취소 요청 DTO 추가 (#48)

---
+++ src/main/java/com/groom/e_commerce/payment/presentation/dto/request/ReqCancelOrderItemPaymentV1.java
@@ -0,0 +1,6 @@
+// src/main/java/com/groom/e_commerce/payment/presentation/dto/request/ReqCancelOrderItemPaymentV1.java
+package com.groom.e_commerce.payment.presentation.dto.request;
+
+public record ReqCancelOrderItemPaymentV1(
+	long cancelAmount
+) {}
