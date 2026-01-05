556712788db4f5e0466ead0a73c6bca3220c1c20
HHLee <hyunho3445@gmail.com>
2026-01-05 12:39:32 +0900

    Revert "fix: User&Seller&AddressEntity 변경"

--- src/main/java/com/groom/e_commerce/ECommerceApplication.java
+++ src/main/java/com/groom/e_commerce/ECommerceApplication.java
@@ -2,13 +2,10 @@ package com.groom.e_commerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class ECommerceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
 }
--- src/main/java/com/groom/e_commerce/user/domain/entity/OwnerStatus.java
+++
@@ -1,7 +0,0 @@
-package com.groom.e_commerce.user.domain.entity;
-
-public enum OwnerStatus {
-    PENDING,    // 승인 대기
-    APPROVED,   // 승인됨
-    REJECTED    // 거절됨
-}
