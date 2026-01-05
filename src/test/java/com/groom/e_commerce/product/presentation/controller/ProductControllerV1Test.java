2e8bb0bda69bb7c98a1ef6eceed6934710ec494b
songhyeonsu1029 <shs032428@gmail.com>
2026-01-05 17:14:26 +0900

    refact : MockitoBean 에노테이션으로 변경

--- src/test/java/com/groom/e_commerce/product/presentation/controller/ProductControllerV1Test.java
+++ src/test/java/com/groom/e_commerce/product/presentation/controller/ProductControllerV1Test.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
@@ -58,16 +58,16 @@ class ProductControllerV1Test {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@MockBean
+	@MockitoBean
 	private JwtUtil jwtUtil;
 
-	@MockBean
+	@MockitoBean
 	private ProductServiceV1 productService;
 
-	@MockBean
+	@MockitoBean
 	private ProductOptionServiceV1 productOptionService;
 
-	@MockBean
+	@MockitoBean
 	private ProductVariantServiceV1 productVariantService;
 
 	@TestConfiguration
