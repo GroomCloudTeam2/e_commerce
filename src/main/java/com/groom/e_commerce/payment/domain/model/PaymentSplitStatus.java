b7c72c087d072db2e22b02ee6357af7c8d3d87a7
yseoy0311 <yseoy0311@gmail.com>
2026-01-06 17:23:23 +0900

    remove(payment): 결제 분할(Split) 관련 Repository 및 상태 모델 제거

--- src/main/java/com/groom/e_commerce/payment/domain/model/PaymentSplitStatus.java
+++
@@ -1,8 +0,0 @@
-// src/main/java/com/groom/e_commerce/payment/domain/model/PaymentSplitStatus.java
-package com.groom.e_commerce.payment.domain.model;
-
-public enum PaymentSplitStatus {
-	PAID,
-	PARTIAL_CANCELLED,
-	CANCELLED
-}
