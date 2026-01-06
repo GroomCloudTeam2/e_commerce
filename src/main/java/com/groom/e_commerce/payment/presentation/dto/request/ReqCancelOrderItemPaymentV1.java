10afa0261a0437ba9056562e6d8bd50ac91f5afe
yseoy0311 <yseoy0311@gmail.com>
2026-01-06 17:21:52 +0900

    remove(payment): 주문상품 단위 결제 취소 요청 DTO 제거

--- src/main/java/com/groom/e_commerce/payment/presentation/dto/request/ReqCancelOrderItemPaymentV1.java
+++
@@ -1,6 +0,0 @@
-// src/main/java/com/groom/e_commerce/payment/presentation/dto/request/ReqCancelOrderItemPaymentV1.java
-package com.groom.e_commerce.payment.presentation.dto.request;
-
-public record ReqCancelOrderItemPaymentV1(
-	long cancelAmount
-) {}
