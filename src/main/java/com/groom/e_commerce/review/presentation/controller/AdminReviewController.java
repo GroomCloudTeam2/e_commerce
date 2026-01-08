a67fe53f82957d466b4d9d7acd2f3425d516e14b
boxty <boxty@boxtyui-MacBookPro.local>
2026-01-08 14:30:42 +0900

    v1

--- src/main/java/com/groom/e_commerce/review/presentation/controller/AdminReviewController.java
+++ src/main/java/com/groom/e_commerce/review/presentation/controller/AdminReviewController.java
@@ -2,10 +2,12 @@ package com.groom.e_commerce.review.presentation.controller;
 
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.groom.e_commerce.review.application.service.ReviewAiSummaryService;
@@ -13,7 +15,7 @@ import com.groom.e_commerce.review.application.service.ReviewAiSummaryService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
-@PreAuthorize("hasRole('MANAGER')")
+@PreAuthorize("hasRole('MASTER')")
 @RestController
 @RequestMapping("/api/v1/admin/reviews")
 @RequiredArgsConstructor
