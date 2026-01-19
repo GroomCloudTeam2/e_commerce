41d9d7bfb4a398820c0df2939bee7d806a7e7a6b
yseoy0311 <yseoy0311@gmail.com>
2026-01-19 18:14:07 +0900

    init

--- src/main/java/com/groom/payment-service/PaymentApplication.java
+++ src/main/java/com/groom/payment-service/PaymentApplication.java
@@ -1,4 +1,4 @@
-package com.groom.e_commerce;
+package com.groom.payment-service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class ECommerceApplication {
+public class PaymentApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
+		SpringApplication.run(PaymentApplication.class, args);
 	}
 }
